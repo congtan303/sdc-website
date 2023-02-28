5b716af2ac7b02e9982020eac583e1886a8a3717
Your Name <you@example.com>
2023-02-28 14:41:05 +0700

    push lai code

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -1,10 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import '../styles/Banner.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import banner1 from '../../src/assets/image/banner/banner1.jpg'
+import banner2 from '../../src/assets/image/banner/banner2.jpg'
+import banner3 from '../../src/assets/image/banner/banner3.jpg'
 const Banner = () => {
   return (
-    <div>
-        
-    </div>
+    <Carousel
+    infiniteLoop={true}
+    autoPlay={true}
+    interval={500}
+    >
+                <div>
+                    <img src={banner1} />
+               
+                </div>
+                <div>
+                    <img src={banner2}/>
+                   
+                </div>
+                <div>
+                    <img src={banner3} />
+                   
+                </div>
+            </Carousel>
   )
 }
 
