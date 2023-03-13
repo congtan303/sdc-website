e8aaf8a7457bc3ddc7f304453ac71767c280af8b
Your Name <you@example.com>
2023-03-13 17:02:35 +0700

    test router 17h

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router basename='/sdc-website'>
+    <Router basename={process.env.PUBLIC_URL || ''}>
       <App />
     </Router>
   </React.StrictMode>
